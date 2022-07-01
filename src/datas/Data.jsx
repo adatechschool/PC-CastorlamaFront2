606faf80a5d11e45067b0d958c195670f50d42d6
charlottengu <98812724+charlottengu@users.noreply.github.com>
2022-07-01 15:11:02 +0200

    feat data

---
+++ src/datas/Data.jsx
@@ -0,0 +1,132 @@
+export const data =  [
+    {
+        id: 1,
+        title: "Chaise en bois",
+        type: "chaise",
+        price: 10,
+        description: "Superbe chaise en bois HYPER confortable",
+        material: [
+            "bois"
+        ],
+        color: "marron",
+        size: "h:90cm l:45cm p:47cm",
+        accepted: false,
+        image_url: "https://res.cloudinary.com/oslanne/image/upload/c_scale,e_auto_contrast,w_500/v1656409611/cld-sample-5.jpg",
+        created_at: "2022-06-29T14:40:27.500Z",
+        edited_at: "2022-06-29T14:40:27.500Z"
+    },
+    {
+        id: 2,
+        title: "Super lit",
+        type: "lit",
+        price: 50,
+        description: "Superbe lit en bois HYPER confortable",
+        material: [
+            "bois"
+        ],
+        color: "bleu",
+        size: "h:60cm l:180cm p:200cm",
+        accepted: true,
+        image_url: "https://res.cloudinary.com/oslanne/image/upload/c_scale,e_auto_contrast,w_500/v1656409586/sample.jpg",
+        created_at: "2022-06-29T14:40:27.500Z",
+        edited_at: "2022-06-29T14:40:27.500Z"
+    },
+    {
+        id: 3,
+        title: "Tabouret",
+        type: "tabouret",
+        price: 15,
+        description: "Superbe tabouret  HYPER confortable",
+        material: [
+            "metal"
+        ],
+        color: "vert",
+        size: "h:90cm l:45cm p:47cm",
+        accepted: false,
+        image_url: "https://res.cloudinary.com/oslanne/image/upload/c_scale,e_auto_contrast,w_500/v1656409610/cld-sample.jpg",
+        created_at: "2022-06-29T14:40:58.153Z",
+        edited_at: "2022-06-29T14:40:58.153Z"
+    },
+
+    {
+        id: 4,
+        title: "Tabouret",
+        type: "tabouret",
+        price: 15,
+        description: "Superbe tabouret  HYPER confortable",
+        material: [
+            "metal"
+        ],
+        color: "vert",
+        size: "h:90cm l:45cm p:47cm",
+        accepted: false,
+        image_url: "https://res.cloudinary.com/oslanne/image/upload/c_scale,e_auto_contrast,w_500/v1656409610/cld-sample.jpg",
+        created_at: "2022-06-29T14:40:58.153Z",
+        edited_at: "2022-06-29T14:40:58.153Z"
+    },
+    {
+        id: 5,
+        title: "Tabouret",
+        type: "tabouret",
+        price: 15,
+        description: "Superbe tabouret  HYPER confortable",
+        material: [
+            "metal"
+        ],
+        color: "vert",
+        size: "h:90cm l:45cm p:47cm",
+        accepted: false,
+        image_url: "https://res.cloudinary.com/oslanne/image/upload/c_scale,e_auto_contrast,w_500/v1656409610/cld-sample.jpg",
+        created_at: "2022-06-29T14:40:58.153Z",
+        edited_at: "2022-06-29T14:40:58.153Z"
+    },
+
+    {
+        id: 5,
+        title: "Tabouret",
+        type: "tabouret",
+        price: 15,
+        description: "Superbe tabouret  HYPER confortable",
+        material: [
+            "metal"
+        ],
+        color: "vert",
+        size: "h:90cm l:45cm p:47cm",
+        accepted: false,
+        image_url: "https://res.cloudinary.com/oslanne/image/upload/c_scale,e_auto_contrast,w_500/v1656409610/cld-sample.jpg",
+        created_at: "2022-06-29T14:40:58.153Z",
+        edited_at: "2022-06-29T14:40:58.153Z"
+    },
+    {
+        id: 6,
+        title: "Tabouret",
+        type: "tabouret",
+        price: 15,
+        description: "Superbe tabouret  HYPER confortable",
+        material: [
+            "metal"
+        ],
+        color: "vert",
+        size: "h:90cm l:45cm p:47cm",
+        accepted: false,
+        image_url: "https://toutchientoutchat.com/wp-content/uploads/2020/04/arbre-%C3%A0-chat-gris-grande-capacit%C3%A9-multi-niveaux-avec-2-niches-hauteur-152-cm.jpg",
+        created_at: "2022-06-29T14:40:58.153Z",
+        edited_at: "2022-06-29T14:40:58.153Z"
+    },
+    {
+        id: 5,
+        title: "Tabouret",
+        type: "tabouret",
+        price: 15,
+        description: "Superbe tabouret  HYPER confortable",
+        material: [
+            "metal"
+        ],
+        color: "vert",
+        size: "h:90cm l:45cm p:47cm",
+        accepted: false,
+        image_url: "https://static.songmics.fr/image/Product/PCT98G/Arbre-a-chat-164-cm-gris-fonce-PCT98G-4.jpg",
+        created_at: "2022-06-29T14:40:58.153Z",
+        edited_at: "2022-06-29T14:40:58.153Z"
+    }
+]
